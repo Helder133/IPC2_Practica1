ba3eaf96068d727cbb9b968cb02ef84c7ea56fa5
Helder133 <hrljuar@gmail.com>
2025-08-11 11:56:13 -0600

    Avanzando con el formulario

--- IPC2_Practica1/src/main/java/ipc2_practica1/ipc2_practica1/Frontend/EventoFrame.java
+++ IPC2_Practica1/src/main/java/ipc2_practica1/ipc2_practica1/Frontend/EventoFrame.java
@@ -6,15 +6,13 @@ package ipc2_practica1.ipc2_practica1.Frontend;
 
 import com.toedter.calendar.JDateChooser;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDesktopPane;
-import javax.swing.JFileChooser;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 
 /**
  *
@@ -25,12 +23,13 @@ public class EventoFrame extends FrameBase {
     private JInternalFrame frameEvento;
 
     public void agregarVentanillaEvento(JDesktopPane jframe, int x, int y) {
-        String title = "Ventanilla de evento";
-        frameEvento = super.agragarVentanilla(jframe, title, x, y);
+        String titulo = "Ventanilla de evento";
+        frameEvento = super.agragarVentanilla(jframe, titulo, x, y);
     }
 
     public void agregarComponentes() {
         Font font1 = new Font("Showcard Gothic", Font.BOLD, 15);
+        Font font2 = new Font("Helvetica", Font.ITALIC, 15);
         
         JLabel codigoEvento = new JLabel("Codigo De Evento: ");
         codigoEvento.setBounds(10, 50, 150, 20);
@@ -39,7 +38,8 @@ public class EventoFrame extends FrameBase {
         
         JTextField textCodigo = new JTextField();
         textCodigo.setBounds(190, 50, 150, 20);
-        textCodigo.setFont(font1);
+        textCodigo.setFont(font2);
+        //validarCargaArchivos(textCodigo);
         frameEvento.add(textCodigo);
         
         JLabel fechaEvento = new JLabel ("Fecha del evento: ");
@@ -48,27 +48,86 @@ public class EventoFrame extends FrameBase {
         frameEvento.add(fechaEvento);
         
         JDateChooser eventoFecha = new JDateChooser();
+        eventoFecha.setFont(font2);
         eventoFecha.setDateFormatString("dd/MM/yyyy");
         eventoFecha.setBounds(190, 90, 150, 20);
+        //validarCargaArchivos1(eventoFecha);
         frameEvento.add(eventoFecha);
         
-        JLabel tipoEvento = new JLabel("Tipo De Evento");
+        JLabel tipoEvento = new JLabel("Tipo De Evento: ");
         tipoEvento.setBounds(10, 130, 150, 20);
         tipoEvento.setFont(font1);
         frameEvento.add(tipoEvento);
         
-        String [] tipo = {"CHARLA", "CONGRESO", "TALLER", "DEBATE"};
+        String [] tipo = {"Seleccionar...","CHARLA", "CONGRESO", "TALLER", "DEBATE"};
         
         JComboBox<String> opciones = new JComboBox(tipo);
         opciones.setBounds(190, 130, 150, 20);
-        opciones.setFont(font1);
+        opciones.setFont(font2);
+        //validarCargaArchivos2(opciones);
         frameEvento.add(opciones);
         
-        opciones.addActionListener((ActionEvent ae) -> {
-            String seleccion = (String) opciones.getSelectedItem();
-            tipoEvento.setText(seleccion);
-        });
         
+        JLabel tituloLabel = new JLabel("Titulo Del Evento: ");
+        tituloLabel.setBounds(10, 170, 150, 20);
+        tituloLabel.setFont(font1);
+        frameEvento.add(tituloLabel);
+        
+        JTextField textTitulo = new JTextField();
+        textTitulo.setBounds(190, 170, 150, 20);
+        textTitulo.setFont(font2);
+        //validarCargaArchivos(textTitulo);
+        frameEvento.add(textTitulo);
+        
+        JLabel ubicacionJLabel = new JLabel("Ubicación: ");
+        ubicacionJLabel.setBounds(10, 210, 150, 20);
+        ubicacionJLabel.setFont(font1);
+        frameEvento.add(ubicacionJLabel);
+        
+        JTextField textUbicacion = new JTextField();
+        textUbicacion.setBounds(190, 210, 150, 20);
+        textUbicacion.setFont(font2);
+        //validarCargaArchivos(textUbicacion);
+        frameEvento.add(textUbicacion);
+        
+        JLabel cupoMaxJLabel = new JLabel("Cupo Máximo: ");
+        cupoMaxJLabel.setBounds(10, 250, 150, 20);
+        cupoMaxJLabel.setFont(font1);
+        frameEvento.add(cupoMaxJLabel);
+        
+        SpinnerNumberModel cupoMax = new SpinnerNumberModel(0,0,1000,1);
+        JSpinner jspinnerCupoMax = new JSpinner(cupoMax);
+        jspinnerCupoMax.setBounds(190, 250, 150, 20);
+        jspinnerCupoMax.setFont(font2);
+        //validarCargaArchivos3(jspinnerCupoMax);
+        frameEvento.add(jspinnerCupoMax);
     }
-
+    
+    /*@Override
+    protected void validarCargaArchivos(JTextField text){
+        if (!super.path.equalsIgnoreCase("")) {
+            text.setEnabled(false);
+        }
+    }
+    
+    @Override
+    protected void validarCargaArchivos1(JDateChooser fecha){
+        if (!super.path.equalsIgnoreCase("")) {
+            fecha.setEnabled(false);
+        }
+    }
+    
+    @Override
+    protected void validarCargaArchivos2(JComboBox<String> opciones){
+        if (!super.path.equalsIgnoreCase("")) {
+            opciones.setEnabled(false);
+        }
+    }
+    
+    @Override
+    protected void validarCargaArchivos3(JSpinner jspiner){
+        if (!super.path.equalsIgnoreCase("")) {
+            jspiner.setEnabled(false);
+        }
+    }*/
 }
--- IPC2_Practica1/src/main/java/ipc2_practica1/ipc2_practica1/Frontend/FrameBase.java
+++ IPC2_Practica1/src/main/java/ipc2_practica1/ipc2_practica1/Frontend/FrameBase.java
@@ -4,21 +4,29 @@
  */
 package ipc2_practica1.ipc2_practica1.Frontend;
 
+import com.toedter.calendar.JDateChooser;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDesktopPane;
 import javax.swing.JFileChooser;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
 
 /**
  *
  * @author helder
  */
 public class FrameBase extends JInternalFrame {
-    
-    public JInternalFrame agragarVentanilla(JDesktopPane jframe, String titulo, int x, int y){
+
+    protected String path = "";
+
+    public JInternalFrame agragarVentanilla(JDesktopPane jframe, String titulo, int x, int y) {
         JInternalFrame ventana = new JInternalFrame(title, true, true, true, true);
         ventana.setLayout(null);
         ventana.setBounds(x, y, 600, 450);
@@ -28,31 +36,68 @@ public class FrameBase extends JInternalFrame {
         agregarComponentes(ventana);
         return ventana;
     }
-    
-    private void agregarComponentes (JInternalFrame frameEvento) {
+
+    private void agregarComponentes(JInternalFrame frameEvento) {
         Font font2 = new Font("Helvetica", Font.BOLD, 15);
-        
-        JLabel rutaArchivo = new JLabel("Ruta archivo...");
+
+        JLabel rutaArchivo = new JLabel("Archivo Seleccionado...");
         rutaArchivo.setFont(font2);
-        rutaArchivo.setBounds(180, 10, 350, 20);
+        rutaArchivo.setBounds(180, 10, 200, 20);
         frameEvento.add(rutaArchivo);
-        
+
         JButton cargaArchivo = new JButton("Carga de archivos");
         cargaArchivo.setBounds(10, 10, 150, 20);
         frameEvento.add(cargaArchivo);
         cargaArchivo.addActionListener(e -> {
             JFileChooser archivo = new JFileChooser();
             archivo.setDialogTitle("Seleccionar un archivo");
-            
+
             int resultado = archivo.showOpenDialog(null);
-            
+
             if (resultado == JFileChooser.APPROVE_OPTION) {
                 java.io.File archivoSeleccionado = archivo.getSelectedFile();
+
+                path = archivoSeleccionado.getAbsolutePath();
+                String nombreArchivo = archivoSeleccionado.getName();
+                rutaArchivo.setText(nombreArchivo);
                 
-                String ruta = archivoSeleccionado.getAbsolutePath();
-                
-                rutaArchivo.setText(ruta);
+                deshabilitarCampos(frameEvento.getContentPane());
             }
         });
+        
+        JButton limpiarEntrada = new JButton("Limpiar...");
+        limpiarEntrada.setBounds(400, 10, 150, 20);
+        frameEvento.add(limpiarEntrada);
+        limpiarEntrada.addActionListener(e -> {
+            rutaArchivo.setText("Archivo Seleccionado...");
+            habilitarCampos(frameEvento.getContentPane());
+        });
     }
+
+    private void deshabilitarCampos(Container container) {
+        for (Component comp : container.getComponents()) {
+            if (comp instanceof JTextField
+                    || comp instanceof JComboBox
+                    || comp instanceof JDateChooser
+                    || comp instanceof JSpinner) {
+                comp.setEnabled(false);
+            } else if (comp instanceof Container container1) {
+                deshabilitarCampos(container1);
+            }
+        }
+    }
+    
+    private void habilitarCampos(Container container){
+        for (Component comp : container.getComponents()) {
+            if (comp instanceof JTextField
+                    || comp instanceof JComboBox
+                    || comp instanceof JDateChooser
+                    || comp instanceof JSpinner) {
+                comp.setEnabled(true);
+            } else if (comp instanceof Container container1) {
+                habilitarCampos(container1);
+            }
+        }
+    }
+
 }
